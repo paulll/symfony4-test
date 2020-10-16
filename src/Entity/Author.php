4852b0a86515f6401fbc5853e68c1deda7c71ecd
paulll <paulll@derpy.ru>
2020-10-16 10:53:04 +0300

    add __toString for book & author entities

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -34,6 +34,11 @@ class Author
         $this->books = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -49,6 +49,11 @@ class Book
         $this->authors = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->title;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
